96f797bd19ad9cb272696a2034ee8bb583f1edf8
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-20 10:11:10 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+import os
+import logging
+from config import Config
+from pyrogram import Client
+
+
+if __name__ == "__main__" :
+    if not os.path.isdir(Config.download_dir):
+        os.makedirs(Config.download_dir)
+    plugins = dict(root="plugins")
+    bot = Client(name="RVX_bot", bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH, plugins=plugins)
+    bot.run()
