c15be429884f14ed071ae7ec4befdd9a8b01da0b
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-20 10:11:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,10 @@ import logging
 from config import Config
 from pyrogram import Client
 
+if not os.path.isdir(Config.download_dir):
+    os.makedirs(Config.download_dir)
+plugins = dict(root="plugins")
+bot = Client(name="RVX_bot", bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH, plugins=plugins)
 
 if __name__ == "__main__" :
-    if not os.path.isdir(Config.download_dir):
-        os.makedirs(Config.download_dir)
-    plugins = dict(root="plugins")
-    bot = Client(name="RVX_bot", bot_token=Config.BOT_TOKEN, api_id=Config.API_ID, api_hash=Config.API_HASH, plugins=plugins)
     bot.run()
