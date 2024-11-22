276c6a00fcf7496bf22af0378c18f536bc3293c2
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 17:20:18 +0530

    Update m_callbacks.py

--- plugins/m_callbacks.py
+++ plugins/m_callbacks.py
@@ -17,8 +17,8 @@ async def _home(client,callback_query):
         else:
             nun=message.chat.first_name
         if nun:
-          st=st.replce("R-user",nun)
-        st=st.replce("user_id",message.chat.id)
+          st=st.replace("R-user",nun)
+        st=st.replace("user_id",message.chat.id)
         reply_msg = await message.edit_text(st)
   else:
         await message.delete();
