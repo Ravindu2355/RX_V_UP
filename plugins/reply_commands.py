6d66c12fc9c7a0e771251b3d10fb316da774e1f6
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 17:25:29 +0530

    Update reply_commands.py

--- plugins/reply_commands.py
+++ plugins/reply_commands.py
@@ -17,7 +17,7 @@ async def _start(client,message:types.Message):
             nun=message.chat.first_name
         if nun:
           st=st.replace("R-user",nun)
-        st=st.replace("user_id",message.chat.id)
+        st=st.replace("user_id",str(message.chat.id))
         reply_msg = await message.reply(st)
     else:
         await message.delete();
