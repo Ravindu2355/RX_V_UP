4b622297a2273fdda1aabca3eb6b7506bdbe8aeb
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 09:08:08 +0530

    Create m_callbacks.py

---
+++ plugins/m_callbacks.py
@@ -0,0 +1,27 @@
+from pyrogram import Client, filters, types
+from Func.reply_text import Text
+
+@app.on_callback_query(filters.callback_data("delm"))
+async def _cancel(client,callback_query):
+  await callback_query.message.delete()
+
+@app.on_callback_query(filters.callback_data("home"))
+async def _home(client,callback_query):
+  message=callback_query.message
+  if str(message.chat.id) in AuthU:
+        un=message.chat.username
+        st=Text.start
+        if un:
+            nun=un
+        else:
+            nun=message.chat.first_name
+        if nun:
+          st=st.replce("R-user",nun)
+        st=st.replce("user_id",message.chat.id)
+        reply_msg = await message.edit_text(st)
+    else:
+        await message.delete();
+
+@app.on_callback_query(filters.callback_data("help"))
+async def _chelp(client,callback_query):
+  await callback_query.message.edit_text(Text.help)
