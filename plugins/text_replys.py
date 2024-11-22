89a89c4df9d3cc1e812a9f557a9fd305437a1caf
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 08:07:43 +0530

    Update text_replys.py

--- plugins/text_replys.py
+++ plugins/text_replys.py
@@ -1,1 +1,24 @@
+#replying commands
+from pyrogram import Client, filters, types
 
+
+@app.on_message(filters.private & filters.command("ping"))
+async def c_pin(client,message:types.Message):
+    start_time = time.time()  # Record start time
+    reply_msg = await message.reply("🔰Ping checking...🔰")
+    end_time = time.time()  # Record end time after sending
+    latency = (end_time - start_time) * 1000  # Convert to milliseconds
+    await reply_msg.edit_text(f"Ping: {latency:.2f} ms")
+
+@app.on_message(filters.private & filters.command("disc"))
+async def c_disc(client,message:types.Message):
+    disk_usage = psutil.disk_usage('/')
+    total_space = disk_usage.total / (1024 ** 3)  # Convert to GB
+    used_space = disk_usage.used / (1024 ** 3)    # Convert to GB
+    free_space = disk_usage.free / (1024 ** 3)    # Convert to GB
+    await message.reply(
+        f"Disk Space:\n"
+        f"Total: {total_space:.2f} GB\n"
+        f"Used: {used_space:.2f} GB\n"
+        f"Free: {free_space:.2f} GB"
+      )
