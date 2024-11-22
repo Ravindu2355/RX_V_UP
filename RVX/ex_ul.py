ff05b3d9e1f4e2f45e9a8f12c7d072a217754f7b
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 22:43:22 +0530

    Update ex_ul.py

--- RVX/ex_ul.py
+++ RVX/ex_ul.py
@@ -1,8 +1,15 @@
 import os
+from pyrogram import Client, filters, types
+import asyncio, os, time, math, psutil
+import json
 from urllib.parse import urlparse
+from bunkr import
+from config import Config
 
-def ex_url(url):
+async def ex_url(app,msg,url,chat_id=):
   ux=urlparse(url)
   nl=u.netloc
-  if nl:
-    
+  if "bunkr" in nl:
+    await ex_bunkr(app,msg,url,chat_id=chat_id)
+  else:
+    await msg.edit_text("Sorry cannot parse that link!")
