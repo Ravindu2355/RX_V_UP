13e7bc9b3694cff3581971379319ee2d87d224ed
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 12:25:37 +0530

    Update simple_func.py

--- Func/simple_func.py
+++ Func/simple_func.py
@@ -12,3 +12,15 @@ def delete_file(file_path):
     else:
         print(f"File '{file_path}' does not exist.")
         return False
+
+def get_file_name_from_response(response):
+    # Check if Content-Disposition header is present
+    content_disposition = response.headers.get('Content-Disposition')
+    if content_disposition:
+        # Extract the filename from the Content-Disposition header
+        filename_part = content_disposition.split('filename=')[-1]
+        filename = filename_part.strip(' "')
+        return filename
+    
+    # Fallback to extracting the file name from the URL
+    return f"video_{str(time.time())}.mp4"
