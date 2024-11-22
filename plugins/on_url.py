90627f63528341e6a828daf001ce7c5c2f3b4480
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-22 17:31:52 +0530

    Update on_url.py

--- plugins/on_url.py
+++ plugins/on_url.py
@@ -8,7 +8,7 @@ from config import Config
 
 @app.on_message(filters.private & filters.regex(pattern=".*http.*"))
 async def handle_tg_ul(client, message: types.Message):
-  if message.chat.id in Config.AuthU:
+  if str(message.chat.id) in Config.AuthU:
     await message.reply(f"Select What You want From Below Uploaders...😎{on_url_bt_text}",reply_markup=on_url_buttons)
   else:
     await message.delete()
