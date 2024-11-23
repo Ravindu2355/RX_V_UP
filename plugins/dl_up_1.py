31cc6b2b21436f94da1e9656f235f1b164aec23e
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-23 09:40:50 +0530

    Update dl_up_1.py

--- plugins/dl_up_1.py
+++ plugins/dl_up_1.py
@@ -4,7 +4,6 @@ from pyrogram import filters, types
 import asyncio, os, time, requests, math, psutil, json
 from moviepy.editor import VideoFileClip
 from PIL import Image
-from bot import app as bot
 from Func.display_progress import progress_for_pyrogram, humanbytes, TimeFormatter
 from Func.cookie import r_cookies, w_cookies, clear_cookies
 from Func.headers import load_headers
@@ -127,7 +126,7 @@ async def upload_from_url(app:Client, chat_id, url: str, n_name=None, n_caption=
             await reply_msg.reply(f"Send Er:No file {filename}")
         await reply_msg.reply(f"Download complete. Generating thumbnail...\nchat_id:{chat_id} filename:{filename}")
         if s_type == "video":
-            s_v = await bot.send_video(
+            s_v = await app.send_video(
                chat_id = int(chat_id),
                video = filename,
                duration=duration,
@@ -139,7 +138,7 @@ async def upload_from_url(app:Client, chat_id, url: str, n_name=None, n_caption=
              )
             fid=s_v.video.file_id
         else:
-            s_v =  await bot.send_document(
+            s_v =  await app.send_document(
                chat_id = int(chat_id),
                document = filename,
                caption=m_caption,
@@ -160,13 +159,13 @@ async def upload_from_url(app:Client, chat_id, url: str, n_name=None, n_caption=
           try:
             if fid is not None:
                 if s_type == "video":
-                    await bot.send_video(
+                    await app.send_video(
                      chat_id=int(Config.M_CHAT),
                      video=fid,
                      caption=f"**Uploaded via RvXBot**"
                    )
                 else:
-                   await bot.send_video(
+                   await app.send_video(
                      chat_id=int(Config.M_CHAT),
                      document=fid,
                      caption=f"**Uploaded via RvXBot**"
