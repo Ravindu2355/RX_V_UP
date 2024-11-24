4f26e5e082f0bcbc3f8d5c2e4e5f83a9a1c679c8
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-24 21:11:01 +0530

    Update buttons.py

--- plugins/buttons.py
+++ plugins/buttons.py
@@ -13,9 +13,9 @@ def make_keyboard(X):
 
 on_url_buttons = make_keyboard(
   [
-    [{"text":"🚀RVX Uploader💪","callback_data":"RVX_uper"}],
+    [{"text":"🚀RVX-Up💪","callback_data":"RVX_uper"}],
     [{"text":"🔰Yt-dlp🔰","callback_data":"ytdlp_uper"}],
-    [{"text":"💯RVX-Extr💥","callback_data":"RVX_Ex"}]
+    [{"text":"💯RVX-Ex💥","callback_data":"RVX_Ex"}]
   ]
 )
 on_url_bt_text = "\n\n🔰**RVX Uploader**:for upload only direct links\n🔰**Yt-dlp**: For eny upload but slow and some time giving errors"
