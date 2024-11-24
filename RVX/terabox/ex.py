0c64e80e7704686c360d2f07ca11607e99549434
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 03:53:53 +0530

    Update ex.py

--- RVX/terabox/ex.py
+++ RVX/terabox/ex.py
@@ -10,18 +10,12 @@ mode=1
 
 def mfiles(data):
     result = []
-    
     for item in data:
-        # Add current item if it's a file (is_dir = 0)
         if item.get('is_dir') == 0:
             result.append(item)
-        
-        # If it's a directory (is_dir = 1), flatten its 'list' recursively
         elif item.get('is_dir') == 1:
             if 'list' in item:
-                # Recursively add items from the directory's list
-                result.extend(flatten_structure(item['list']))
-    
+                result.extend(mfiles(item['list']))    
     return result
 
 def getf():
@@ -29,17 +23,11 @@ def getf():
     url = 'https://www.terabox.app/wap/share/filelist?surl=GJACgid49fFsWkxtfVH3cA'
     get_file_url = f'{api}/generate_file'  # Replace 'api' with the appropriate variable
     headers = {'Content-Type': 'application/json'}
-    
-    # Define the data payload
     data = {
         'url': url,
         'mode': mode  # Replace 'mode' with the appropriate variable
     }
-    
-    # Make the POST request
     response = requests.post(get_file_url, headers=headers, json=data)
-    
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
         result = response.json()  # Parse the response as JSON
         print(json.dumps(result, indent=4))  # Pretty print the JSON response
@@ -49,7 +37,6 @@ def getf():
         return None
 
 def dlg(uk, sid, ts, sign, jt, c, fid):
-    # Define the parameters for the request
     param = {
       'mode'      : mode,
       'uk'        : uk,
