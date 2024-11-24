6d786e13432275e2233c1993e4ffc40fa418da4b
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-24 16:22:32 +0530

    Update globals.py

--- globals.py
+++ globals.py
@@ -3,4 +3,16 @@ run = 0 # 0- defines bot was free now.... 1-was runing on task....
 progress_s = "free" #set defalt as free.....
 multi_t = 0 #multi task count somehelp for add more tasks to bot...
 tasks={}
-task_help={}
+task_help={
+  "bunkr":{
+    headers:{
+      "Referer": "https://bunkr.ph/"
+    },
+  },
+  "DESI":{
+    
+  },
+  "":{
+    
+  }
+}
