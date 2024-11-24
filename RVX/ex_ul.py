4c62b06b02b67144d5868c5df49b125069c0855f
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 04:19:15 +0530

    Update ex_ul.py

--- RVX/ex_ul.py
+++ RVX/ex_ul.py
@@ -8,9 +8,10 @@ from RVX.terabox.ex import tera
 from config import Config
 
 async def ex_url(app,msg,url,chat_id=Config.M_CHAT):
+  await msg.edit_text("🔰Processing!....🚀")
   if "bunkr" in url:
     await ex_bunkr(app,msg,url,chat_id=chat_id)
   elif "tera" in url:
     await tera(app, msg, url, chat_id=chat_id)
   else:
-    await msg.edit_text("Sorry cannot parse that link!")
+    await msg.edit_text("Sorry cannot parse that link!🤕")
