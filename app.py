a307ec2560f779a978a38121de093c539894520d
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 12:39:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ CORS(app)
 def hello_world():
     return 'Hello from Koyeb'
     
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000)
+app.run(host='0.0.0.0',port=8000)
+#if __name__ == "__main__":
+    
