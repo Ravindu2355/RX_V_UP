f44868bce3bd0e31b1fbeb56c1bba062d5a9e397
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-25 11:10:48 +0530

    Update reply_text.py

--- Func/reply_text.py
+++ Func/reply_text.py
@@ -1,4 +1,9 @@
 class Text(object):
-  start = "🔰**RVX Uploader**🔰\n\n❤️Hi [R-user](tg://user?id=user_id) ..🫡\nI'm Url Uploader bot.\nMade by Rvx..."
-  help = ""
+  start = "🔰**RVX Uploader**🔰\n\n❤️Hi [R-user](tg://user?id=user_id) ..🫡\nI'm Url Uploader bot🤖.\nMade by Rvx...\nAnd I am a 💯multi functional💫 bot that make uploading very eazy😏 and quic😇 and maybe advanced😍\nBecause I had some 🪚extractin method🛠 somepopuler bot/sites don't now😎..."
+  help = {
+    "help_main":"**🔰RVX-Help🔰**\n\nThis bot for download any file from url🔗 to telegram📱\n\n This us simple help for it\nExplore the pages below for Help and know how to work with this bot!🗝🔑",
+    "help_ct_H":"",
+    "help_ct_C":"",
+    "help_":"",
+  }
   
